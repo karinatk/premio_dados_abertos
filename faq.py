dc5d609d91b1bb804a780f19aa88d2bcf0830e1d
Karina Kato <karina.kato@ifood.com.br>
2021-08-21 10:44:16 -0300

    added dataframe class

--- faq.py
+++ faq.py
@@ -1,23 +1,58 @@
 import streamlit as st
+import pandas as pd
+
+def group_contracts(data):
+    agg_dict = {column: ('first' if column != 'valor_contratado' else 'sum') for column in data.columns}
+    grouped_contracts_df = data.groupby(['CNPJ', 'Número do contrato'], as_index=False).agg(agg_dict)
+    return grouped_contracts_df
+
+def pre_process_continuous(data):
+    processed_data = data.copy()
+    processed_data['mes_contratado'] = processed_data['Data da contratação'].str.split('/').str[1].astype(int)
+    processed_data['ano_contratado'] = processed_data['Data da contratação'].str.split('/').str[2].astype(int)
+    processed_data['valor_contratado'] = processed_data['Valor contratado  R$'].str.replace('.', '').astype(float)
+    processed_data['prazo_carencia'] = processed_data['Prazo - carência (meses)']
+    processed_data['prazo_amortizacao'] = processed_data['Prazo - amortização (meses)']
+    processed_data['juros'] = processed_data['Juros'].str.replace(',', '.')
+    processed_data['porte_do_cliente'] = processed_data['Porte do cliente'].apply(get_size_of_company_label)
+    return processed_data
+
+def get_size_of_company_label(size_of_company):
+    if size_of_company == 'MICRO':
+        label = 0
+    elif size_of_company == 'PEQUENA':
+        label = 1
+    elif size_of_company == 'MÉDIA':
+        label = 2
+    elif size_of_company == 'GRANDE':
+        label = 3
+    else:
+        label = 4
+    return label
+
+@st.cache(persist=True)
+def load_data():
+    operacoes = pd.read_csv('csv/Operações contratadas na forma direta e indireta não automática (2002 a 30.06.2021).csv', sep=';', encoding='latin-1')
+    processed_data = pre_process_continuous(operacoes)
+    grouped_contracts_df = group_contracts(processed_data)
+    return grouped_contracts_df
 
 class Faq: 
 
     def __init__(self) -> None:
         self.pages = []
+        self.grouped_contracts_df = load_data()
     
     def add_page(self, title, func) -> None: 
-
         self.pages.append({
                 "title": title, 
                 "function": func
             })
 
     def run(self):
-        # Drodown to select the page to run  
         page = st.sidebar.selectbox(
-            'Selecione a página', 
+            "Selecione a página", 
             self.pages, 
-            format_func=lambda page: page['title']
+            format_func=lambda page: page["title"]
         )
-
-        page['function']()
+        page["function"](self.grouped_contracts_df)
--- paginas_faq/tipo_financiamento.py
+++ paginas_faq/tipo_financiamento.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-def get_page_information():
+from dataframe import BndesDataframe
+
+def get_page_information(grouped_contracts_df):
     st.title("O que pode ser financiado?")
 
     st.write(
@@ -13,3 +15,6 @@ def get_page_information():
         Em geral, são realizadas na forma de apoio indireto todas as operações de financiamento à compra isolada de máquinas e equipamentos, bem como financiamentos inferiores a R$ 10 milhões destinados a projetos de implantação, modernização e expansão de empreendimentos.
         Para solicitar apoio direto ao BNDES, é necessário, em geral, que o financiamento tenha valor superior a R$ 40 milhões - R$ 20 milhões a depender da sistemática. Em alguns casos específicos, o BNDES permite o apoio direto a financiamentos de valor inferior a esse limite.
     """)
+
+    df = BndesDataframe(grouped_contracts_df, grouped_contracts_df.columns)
+    st.plotly_chart(df.get_pie_chart("Setor BNDES"), use_container_width=True)
