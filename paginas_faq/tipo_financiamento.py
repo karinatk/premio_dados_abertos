6e2a5f4da341d416d7f52b4ba662a594d3c9c813
Karina Kato <karina.kato@ifood.com.br>
2021-08-21 15:45:31 -0300

    updated created plots and description

--- paginas_faq/tipo_financiamento.py
+++ paginas_faq/tipo_financiamento.py
@@ -10,38 +10,87 @@ def get_page_information(cached_df):
         O BNDES financia a quase totalidade dos setores da economia, dentre eles: infraestrutura; indústria, comércio e serviços;
         agropecuária; exportação; desenvolvimento regional e territorial; cultura e economia criativa; entre outros.
 
-        A seguir você consegue visualizar interativamente a porcentagem de financiamentos por setor, podendo ser filtrado por ano ou removido algum setor.
+        A seguir você consegue visualizar interativamente a porcentagem de financiamentos por setor, podendo ser escolhido o tipo de classificação de setor, o tipo de agregação dos dados e filtros de intervalos de anos.
         ##
-
     """)
 
-    row1_1, row1_2 = st.columns((1,1))
+    row1_1, row1_2, row1_3 = st.columns((1, 1, 1))
+    sector_classification_type = "BNDES"
 
     with row1_1:
-        sector_type = st.radio(
-            "Escolha o tipo de setor",
+        sector_classification_type = st.radio(
+            "Tipo de classificação de setor",
             ("BNDES", "CNAE")
         )
+        sector_column = "Setor {}".format(sector_classification_type)
 
     with row1_2:
+        aggregation_type = st.radio(
+            "Tipo de agregação",
+            ("Quantidade de projetos", "Valor financiado")
+        )
+        use_sum = True if aggregation_type == "Valor financiado" else False
+
+    with row1_3:
         min_year, max_year = st.slider("Filtro por ano", 2002, 2021, (2002, 2021))
 
-    boolean_series = (cached_df['ano_contratado'] >= min_year) & (cached_df['ano_contratado'] <= max_year)
-    filtered_df = cached_df[boolean_series]
+    st.write("##")
+    st.title("O que já financiamos?")#"Classificação de setor do {}".format(sector_classification_type))
+    st.write(
+    """
+        As seguintes visualizações são interativas e geradas a partir dos filtros passados anteriormente.
+        Nos gráficos a seguir você poderá conferir informações de {} nos anos de {} a {} por setor.
+        Caso deseje remover algum setor da visualização, apenas clique no nome do setor.
+    """.format(aggregation_type.lower(), min_year, max_year)
+    )
+
+    boolean_series = (cached_df["ano_contratado"] >= min_year) & (cached_df["ano_contratado"] <= max_year)
+    filtered_by_year_df = cached_df[boolean_series]
 
     st.plotly_chart(
         plots.get_pie_chart(
-            filtered_df,
-            "Setor {}".format(sector_type),
-            title='Visualização interativa de setor financiado nos anos de {} a {}'.format(min_year, max_year)),
-        use_container_width=True)
+            filtered_by_year_df,
+            sector_column,
+            title="Porcentagem de {} por setor nos anos de {} a {}".format(aggregation_type.lower(), min_year, max_year),
+            use_sum=use_sum
+            ),
+        use_container_width=True
+    )
 
-    if sector_type == "CNAE":
-        sector_type = sector_type + " agrupado"
+    st.plotly_chart(
+        plots.get_histogram(filtered_by_year_df, sector_column, use_sum=use_sum),
+        use_container_width=True
+    )
+
+    st.write(filtered_by_year_df)
+
+    sector_type = st.radio(
+        "Escolha um setor",
+        filtered_by_year_df[sector_column].unique().tolist()
+    )
+
+    if sector_classification_type == "CNAE":
+        sector_classification_type += " agrupado"
+    subsector_column = "Subsetor {}".format(sector_classification_type)
+
+    st.write("##")
+    st.title("{} - {}".format(sector_column, sector_type))
+
+    filtered_by_sector_df = filtered_by_year_df[filtered_by_year_df[sector_column] == sector_type]
 
     st.plotly_chart(
         plots.get_pie_chart(
-            filtered_df,
-            "Subsetor {}".format(sector_type),
-            title='Visualização interativa de subsetor financiado nos anos de {} a {}'.format(min_year, max_year)),
-        use_container_width=True)
+            filtered_by_sector_df,
+            subsector_column,
+            title="Visualização interativa por subsetor financiado nos anos de {} a {}".format(min_year, max_year),
+            use_sum=use_sum
+            ),
+        use_container_width=True
+    )
+
+    st.plotly_chart(
+        plots.get_histogram(filtered_by_sector_df, subsector_column, use_sum=use_sum),
+        use_container_width=True
+    )
+
+    st.write(filtered_by_sector_df)
